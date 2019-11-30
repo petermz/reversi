f2e39d882251457053820c97dcf0ce79fc9d3c40
peterz <peterz@disroot.org>
2019-11-30 12:06:15 +0300

    IDEA project + Gtk3 boilerplate

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,16 @@
 module Main where
 
+import Control.Monad.IO.Class
+import Graphics.UI.Gtk
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+  initGUI
+  
+  window <- windowNew
+  set window [ windowTitle := "Reversi" ]
+  window `on` deleteEvent $ liftIO mainQuit >> return False
+    
+  widgetShowAll window
+  mainGUI
